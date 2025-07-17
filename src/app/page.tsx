25ec39779155f7ef2ac993189bfc01ca7831eb81
chaitanya-code24 <chaitanyalokhande2412work@gmail.com>
2025-07-18 02:24:49 +0530

    made some changes in UI/UX

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import React from "react";
-import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { auth } from "./firebase";
